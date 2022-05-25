335da1d46f1456bd591b966bfc309a234740a144
Echoxiaoliu <1048750311@qq.com>
2022-05-25 21:37:04 +0800

    完成修改后端vercel搭建

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
         proxy: {//解决跨域问题
             '/api': {
                 // 此处的写法，目的是为了 将 /api 替换成 https://autumnfish.cn/
-                target: 'http://119.29.86.152:3001/',
+                target: 'https://netease-cloud-music-api-blue-three-64.vercel.app/',
                 // target: 'http://m.kugou.com/',
                 // target: 'http://mobilecdnbj.kugou.com/v3',
                 // 允许跨域
