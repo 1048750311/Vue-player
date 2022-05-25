39ba9e5a6703fe1c7434c1574a52c0f6d0592d69
Echoxiaoliu <1048750311@qq.com>
2022-05-25 18:15:57 +0800

    vue-player上线

---
+++ src/components/common/friend/Friends.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="friends">
+      <h1 style="fontSize:22px;fontWeight:bold;textAlign:center;marginTop:20px">我的朋友</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Friends",
+}
+</script>
+
+<style scoped>
+
+</style>
